f3b578eb0848bcc16223a967d1ead9264963b2dc
spromrm <r.mamukashvili@gmail.com>
2022-03-06 15:27:55 +0400

    Task 1 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React,{ useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { gsap } from "gsap";
 
 function App() {
+
+  const el = useRef();
+  const q = gsap.utils.selector(el);
+  
+  useEffect(() => {
+    
+    gsap.to(q(".item"), {
+      x: 50,
+      stagger: 0.33,
+      repeat: -1,
+      repeatDelay: 1,
+      yoyo: true
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +34,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <div className='items' ref={el}>
+          <div className='item'>item 1</div>
+          <div className='item'>item 2</div>
+          <div className='item'>item 3</div>
+          <div className='item'>item 4</div>
+          <div className='item'>item 5</div>
+        </div>
+      </div>
     </div>
   );
 }
