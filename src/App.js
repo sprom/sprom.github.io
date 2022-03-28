d53f2a90c678baa14c0bc6ce2335037760bbac7f
spromrm <r.mamukashvili@gmail.com>
2022-03-28 18:13:34 +0400

    coments deleted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
-import { createBrowserHistory } from "history";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Switch } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 import { Route, Link, BrowserRouter } from "react-router-dom";
@@ -13,12 +12,9 @@ import PageNotFound from "./pages/pagenotfound/pagenotfound";
 import Search from "./pages/search/search";
 import Logo from "./resources/img/logo.svg";
 function App() {
-  const [inputval, setInputVal] = useState("");
-  // console.log(inputval);
-  const history = createBrowserHistory({
-    basename: process.env.PUBLIC_URL,
-  });
+
   const dispatch = useDispatch();
+
   const wetherApi =
     "https://api.openweathermap.org/data/2.5/weather?lat=41.6934591&lon=44.8014495&appid=c36856ae34b4e3e7fb9c1d1556dd2992";
 
@@ -40,37 +36,13 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        {/* <button
-          onClick={() =>
-            dispatch({
-              type: "INCRIMENT",
-              payload:5
-            })
-          }
-        >
-          dddd
-        </button> */}
-        {/* {weather} */}
-        {/* {
-          inputval? <Redirect to={"/search/"+inputval} />:''
-          }
-       
-          <Route path="/">
-            <Search change={setInputVal} />
-          </Route>
-
-          <Route  path="/search/:id">
-            <Listing />
-          </Route> */}
         <MainMenu />
-
         <div className="content">
           <div className="logo">
             <Link to={"/"}>
               <img src={Logo} alt="logo" />
             </Link>
           </div>
-
           <Switch>
             <Route exact={true} path="/">
               <HomePage />
